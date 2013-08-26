d07907fb1c2c1ad0289c74384570a9f628dae386
Tmeister <noone@tmeister.net>
2013-08-25 21:14:28 -0500

    Added AutoUpdates for store

--- sections/collapser/section.php
+++ sections/collapser/section.php
@@ -27,8 +27,8 @@ class CollapserTm extends PageLinesSection
         $this->post_type_setup();
         $this->post_meta_setup();
         $this->section_key = strtolower( str_replace(' ', '_', $this->section_name) );
-        //$this->verify_license();
-        //add_filter('pl_sorted_settings_array', array(&$this, 'add_global_panel'));
+        $this->verify_license();
+        add_filter('pl_sorted_settings_array', array(&$this, 'add_global_panel'));
     }
 
      function verify_license(){
