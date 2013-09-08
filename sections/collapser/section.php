edda1f256de0c3dee46b76a6a175f600866f3a92
Enrique Chavez <noone@tmeister.net>
2013-09-07 19:30:47 -0500

    Add apply_filters to support short codes
    Signed-off-by: Enrique Chavez <noone@tmeister.net>

--- sections/collapser/section.php
+++ sections/collapser/section.php
@@ -280,7 +280,7 @@ class CollapserTm extends PageLinesSection
                     </div>
                     <div id="'.$parent.'-'.$post->ID.'" class="accordion-body collapse '.$in.'">
                       <div class="collapser-inner">
-                        <p>'.nl2br($post->post_content).'</p>
+                        <p>'.apply_filters('the_content' ,$post->post_content).'</p>
                         '.$morelink.'
                       </div>
                     </div>
